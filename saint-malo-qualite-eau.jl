e5c13aabbe3044182852456710546d27a1da3abf
Colin Fay <contact@colinfay.me>
2017-08-13 21:19:11 +0200

    Add files via upload

---
+++ saint-malo-qualite-eau.jl
@@ -0,0 +1,115 @@
+using DataFrames
+df = readtable("mesures-de-la-qualite-de-lair.csv")
+names(df)
+describe(df)
+summary(df)
+using Gadfly
+using Cairo
+using Fontconfig
+Gadfly.push_theme(:default)
+p = plot(df,
+         x = :Année,
+         y = :Ozone_O3_moyenne_annuelle_µg_m3,
+        Geom.point,
+        Geom.line,
+        Guide.title("Ozone (O3) - moyenne annuelle µg/m3 à Saint Malo"),
+        Guide.xlabel("Année"),
+        Guide.ylabel("Ozone"),
+        Theme(background_color = "white",
+                  major_label_font="CMU Serif",
+                  major_label_font_size = 25pt,
+                  minor_label_font_size = 20pt,
+                  line_width = 5pt,
+                  point_size = 7pt,
+                  default_color=color("#973232")))
+img = PNG("Ozone_O3_moyenne_annuelle_µg_m3.png", 16inch, 8inch)
+draw(img, p)
+
+p = plot(df,
+         x = :Année,
+         y = :Ozone_O3_maximum_horaire,
+        Geom.point,
+        Geom.line,
+        Guide.title("Ozone (O3) - maximum horaire µg/m3 à Saint Malo"),
+        Guide.xlabel("Année"),
+        Guide.ylabel("Maximum horaire"),
+        Theme(background_color = "white",
+        major_label_font="CMU Serif",
+        major_label_font_size = 25pt,
+        minor_label_font_size = 20pt,
+        line_width = 5pt,
+        point_size = 7pt,
+                  default_color=color("#1E5B5B")))
+img = PNG("ozone_annuel_saint_malo.png", 16inch, 8inch)
+draw(img, p)
+
+p = plot(df,
+         x = :Année,
+         y = :Ozone_O3_jours_de_dépassement_du_seuil_d_information,
+        Geom.bar,
+        Guide.title("Ozone (O3) - jours de dépassement du seuil d'information"),
+        Guide.xlabel("Année"),
+        Guide.ylabel("Jours de dépassement dans l'année"),
+        Theme(background_color = "white",
+        major_label_font="CMU Serif",
+        major_label_font_size = 25pt,
+        minor_label_font_size = 20pt,
+        line_width = 5pt,
+        point_size = 7pt,
+                  default_color=color("#6D8D2F")))
+img = PNG("Ozone_O3_jours_de_dépassement_du_seuil_d_information.png", 16inch, 8inch)
+draw(img, p)
+
+p = plot(df,
+         x = :Année,
+         y = :Dioxyde_d_azote_NO2_moyenne_annuelle_µg_m3,
+        Geom.point,
+        Geom.line,
+        Guide.title("Dioxyde d'azote (NO2) - moyenne annuelle µg/m3 à Saint Malo"),
+        Guide.xlabel("Année"),
+        Guide.ylabel("Dioxyde d'azote"),
+        Theme(background_color = "white",
+        major_label_font="CMU Serif",
+        major_label_font_size = 25pt,
+        minor_label_font_size = 20pt,
+        line_width = 5pt,
+        point_size = 7pt,
+                  default_color=color("#287928")))
+img = PNG("Dioxyde_d_azote_NO2_moyenne_annuelle_µg_m3.png", 16inch, 8inch)
+draw(img, p)
+
+p = plot(df,
+         x = :Année,
+         y = :Dioxyde_d_azote_NO2_maximum_horaire,
+        Geom.point,
+        Geom.line,
+        Guide.title("Dioxyde d'azote (NO2) - maximum horaire à Saint Malo"),
+        Guide.xlabel("Année"),
+        Guide.ylabel("Maximum horaire"),
+        Theme(background_color = "white",
+        major_label_font="CMU Serif",
+        major_label_font_size = 25pt,
+        minor_label_font_size = 20pt,
+        line_width = 5pt,
+        point_size = 7pt,
+                  default_color=color("#E18C8C")))
+img = PNG("Dioxyde_d_azote_NO2_maximum_horaire.png", 16inch, 8inch)
+draw(img, p)
+
+p = plot(df,
+         x = :Année,
+         y = :Dioxyde_d_azote_NO2_jours_de_dépassement_du_seuil_d_information,
+        Geom.bar,
+        Guide.title("Dioxyde d'azote (NO2) - jours de dépassement du seuil d'information"),
+        Guide.xlabel("Année"),
+        Guide.ylabel("Jours de dépassement"),
+        Theme(background_color = "white",
+        major_label_font="CMU Serif",
+        major_label_font_size = 25pt,
+        minor_label_font_size = 20pt,
+        line_width = 5pt,
+        point_size = 7pt,
+
+                  default_color=color("#548787")))
+img = PNG("Dioxyde_d_azote_NO2_jours_de_dépassement_du_seuil_d_information.png", 8inch, 4inch)
+draw(img, img)
